226e02a4a729132cc74f3e8d4d1ce3417299dcad
xenia nikitina <XeniaNikitina>
2018-05-19 16:42:51 +0300

    Frogbotcommit1

---
+++ Bot_telegram.py
@@ -0,0 +1,32 @@
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
+    'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
+
+logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log'
+                    )
+
+def greet_user(bot, update):
+   text = 'Hello Man!'
+   print(text)
+   update.message.reply_text(text)
+
+def talk_to_me(bot, update):
+    user_text = update.message.text 
+    print(user_text)
+    update.message.reply_text(user_text)
+
+def main():
+    mybot = Updater("616636802:AAGawBQlfH_8RqTcwYCJJVVvu8V2EeHwYMs" , request_kwargs=PROXY)
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    mybot.start_polling()
+    mybot.idle()
+
+   
+
+main()
